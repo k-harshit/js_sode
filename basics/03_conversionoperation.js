399aee254afbdb5f563315be8302d7848d9fefca
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-10 19:31:03 +0000

    changes_for_today

---
+++ basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+//let numberValue= Number (score)
+//console.log(typeof numberValue);
+//console.log(numberValue);
+//numberValue is NaN ->Not a Number
+/*
+"33 " ==> 33
+"abs34" ==> NaN
+true  ==> 1
+*/
+
+let myName ="papa"
+let bool_name=Boolean(myName)
+console.log(bool_name);
+/*
+1 => true ; 0=>false;
+ " " => false;
+ "abcd" => true;
+ */
