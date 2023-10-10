dd14853cb82cd4b989e8d82f32b12f0b85d03b92
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-10 19:03:11 +0000

    changed

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id = 638633;
+let  account_eMail = "singhk_harshit@gmail.com"
+ var acc_Pass = "vinod_chopra"
+   account_City = "varanasi" // not a good way to declare a variable( declare type of variable)
+
+let address;
+   account_eMail= "ab@cd.com"
+   acc_Pass = "23454"
+   account_City = "begaluru"
+   console.table([account_Id,account_eMail,acc_Pass,account_City,address])
+   console.log(address);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" ; //treat all js code as newer version of js code
+
+//alert("hello")  // using nodejs not browser
+
+//DATA tpyes in js
+/* number => 2^53
+string =>"  "
+boolean => true/false
+null =>empty value
+undefined => 
+symbol => unique
+
+// object
+
+*/
