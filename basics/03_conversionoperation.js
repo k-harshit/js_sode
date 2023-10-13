61ce55796e23e1b92c0c949419dbbfad6898956d
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-13 19:15:44 +0000

    changed

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -12,11 +12,42 @@ let score = null
 true  ==> 1
 */
 
-let myName ="papa"
-let bool_name=Boolean(myName)
+let myName ="" 
+let bool_name=Boolean(myName) // agar myame me koi string hoga toh 1 otherwise 0
 console.log(bool_name);
 /*
 1 => true ; 0=>false;
  " " => false;
  "abcd" => true;
  */
+/*******                     operations                            *******/
+
+
+console.log("1" + 2 ) 
+console.log(1 + "2" )
+console.log(1 + 1 + "2");
+console.log("1" + 1 + 2 );
+
+
+console.log(true)
+console.log(+true);
+console.log(" ");
+console.log(+"");
+// == & <,>,<=,>=
+// i.e checking and comparison operators work differently in js
+
+/*
+= is an assignment operator which is used to assign a value to a variable.
+== is an equality operator which is used to compare the values of two variables.
+=== is an strictly equality operator which is used to compare the values as well as the type of the two variables.
+*/
+console.log(null > 0);
+console.log(null < 0 );
+console.log(null >= 0);
+// null is converted into NaN/0 ambiguously so it is a problem
+//           OR
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+// both null & undefined----- above type of code should be avoided
---
+++ basics/04_dataTypes.js
@@ -0,0 +1,28 @@
+/**
+ * 2 Types 
+ * --> Primitive
+ * String
+ * null
+ * undefined
+ * Symbol
+ * BigInt
+ * Boolean
+ * Number
+
+ * -->Reference/non-primitive
+Array
+Object
+Functions
+
+ */
+
+let name_of_heros = ["iron mano","thomas shelby","Joker"]
+let myObj={
+    name:"harshit",
+    age:22
+}
+console.log(myObj);
+
+const myFunction = function name() {
+    console.log("Good morning sir");
+}
