5a78b8f1449f19b84927c73c9018c31f124f0e02
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-19 17:36:11 +0000

    commit success

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -5,3 +5,17 @@ const name='harshit';
 // console.log(name  + " repo_count " + repo_count);
 // above way of displaying info isn't recommended anymore instead use below depicted way
 console.log(`my name is ${name} and my repo count value is ${repo_count}`);
+const gameName = new String('harshit')
+console.log(gameName);
+console.log(gameName.__proto__); 
+/* there are various functions inbuilt in string [refer MDN]*/
+/**
+ * charAt
+ * indexOf
+ * substring
+ * slice
+ * trim
+ * etc
+ * to search keywords (includes())
+ * 
+ * /
