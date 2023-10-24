bb167c18831ff268a5f6cbcfbbf3179b0faeff6f
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-24 15:12:25 +0000

    changed

---
+++ basics/07_dates.js
@@ -0,0 +1,36 @@
+let my_date = new Date()
+// console.log(my_date);
+// console.log(my_date.toString());
+// console.log(my_date.toISOString());
+// console.log(my_date.toJSON());
+// console.log(my_date.toLocaleDateString());
+// console.log(my_date.toLocaleString());
+// console.log(my_date.toTimeString());
+// console.log(my_date.toUTCString());
+// console.log(my_date.toDateString());
+
+
+// let my_created_date = new Date(2023,22,3)
+// console.log(my_created_date.toLocaleString());
+
+
+// let my_new_date=("2023-03-24")
+// console.log(my_new_date);
+
+
+
+////
+// let another_time_s=Date.now()
+// console.log(Math.floor(another_time_s/1000));
+// let mytime_stamp=Date.now()
+// console.log(Math.floor(mytime_stamp/1000));
+
+/**----------------------------------------* */
+
+
+let new_date = new Date();
+console.log(new_date);
+console.log(new_date.getFullYear())
+console.log(new_date.getMonth());
+console.log(new_date.getDay());
+console.log(new_date.getMinutes());
