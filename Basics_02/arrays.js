8a9bdd1fc6f8b7bbdfc75cebb2e0148c6df91c0a
k-harshit <34953526+k-harshit@users.noreply.github.com>
2023-10-26 19:18:37 +0000

    changed

---
+++ Basics_02/arrays.js
@@ -0,0 +1,19 @@
+// splice is used to delete the elements indicated from the original Array
+
+const an_array=[1,2,5,[3,4,5,[4,5,6,[56,78,8]]]]
+console.log(an_array.flat(Infinity));
+
+
+/** flat(depth) is used to present an layered array in the form of a single array
+
+*/
+console.log(Array.isArray(/*string to be chkd to be array */));
+ console.log( Array.from("Harshit") );
+ console.log(Array.from({name : "Harshit"}));  // important for interview
+
+
+ //------------------------------------------------------------
+
+
+ const a=b=c=d=e=f=12;
+console.log(Array.of(a,b,c,d,e,f));
